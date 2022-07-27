51a6c0bca89d1665a8287de33a44564d27c9193e
Sumit Nimbarte <smtnimbarte499@gmail.com>
2022-07-27 23:28:02 +0530

    recursion

---
+++ recursion.js
@@ -0,0 +1,21 @@
+// write a function which accepts base and exponent and calculate power
+
+function power(base, exponent) {
+  if (exponent === 0) return 1;
+  return base * power(base, exponent - 1);
+}
+
+// console.log(power(2, 0));
+// console.log(power(2, 1));
+// console.log(power(2, 2));
+// console.log(power(2, 3));
+
+function fib(n) {
+  if (n <= 2) return 1;
+  return fib(n - 1) + fib(n - 2);
+}
+
+console.log(fib(4)); // 3
+console.log(fib(10)); // 55
+console.log(fib(28)); // 317811
+console.log(fib(35)); // 9227465
