a7afe056ef5b771ec4725cadaaf6e316b30c6384
Sumit Nimbarte <smtnimbarte499@gmail.com>
2023-07-28 01:46:29 +0530

    Implement merge sort

--- mergeSort.js
+++ mergeSort.js
@@ -28,4 +28,18 @@ const merge = (arr1,arr2)=>{
 }
 
 
-console.log(merge([1,23],[2,34,45,69]))
+const mergeSort = (arr)=>{
+
+    if(arr.length <= 1) return arr;
+
+    const mid = Math.floor(arr.length/2);
+
+    const left = mergeSort(arr.slice(0,mid));
+    const right = mergeSort(arr.slice(mid));
+
+    return merge(left,right);
+    
+}
+
+console.log(merge([1,23],[2,34,45,69]));
+console.log(mergeSort([1,23,2,4,1,35,32,1,1]));
