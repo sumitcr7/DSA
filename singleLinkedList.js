098996b62012d53140bf5037f4d5eb7a4607bfe6
Sumit Nimbarte <smtnimbarte499@gmail.com>
2023-08-15 22:26:40 +0530

    Implement unshift method
    Unshift method is used to add node at the start of the  list and it shift the current head to one position forward

--- singleLinkedList.js
+++ singleLinkedList.js
@@ -57,6 +57,18 @@ class SinlgeLinkList {
         return current;
         
     }
+
+    unshift(val){
+        var newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        }else {
+        newNode.next = this.head;
+        this.head = newNode;}
+        this.length++;
+        return this;
+    }
 }
 
 
