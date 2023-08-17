8887581f74f918816fd3cc9f58d66a20128baec1
Sumit Nimbarte <smtnimbarte499@gmail.com>
2023-08-17 23:55:47 +0530

    Create class for Node and Doubly Linked List

---
+++ doublyLinkedList.js
@@ -0,0 +1,15 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = null;
+    }
+}
